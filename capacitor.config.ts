3b8f96db2ee935f470df61d2e877aeef08031a97
anisadleena <anisadleena192@gmail.com>
2024-03-16 12:01:50 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-recipes',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
